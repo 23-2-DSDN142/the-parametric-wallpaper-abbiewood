e46d4732c26e21cb9d62e6a360b7615e711b94ff
abbiewood <139762394+abbiewood@users.noreply.github.com>
2023-08-03 12:08:53 +1200

    Added If statements and variables

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,16 +1,38 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-let flowerX = 150;
-let flowerY = 150;
-let petalSize = 100;
-let centerPetal = petalSize / 1.2;
-let petalDistance = petalSize / 2;
+
+//centre + details
+let centreX = 50 // flower centre
+let centreY = 50 
+let centreSize = 30
+
+//centre circle details
+let detailX = 53
+let detailY = 40
+let circleSize = 5 //small circles around perimeter of centre
+
+//main petals
+let petalSize = 35 
+let mainpetalX = 50
+let mainpetalY = 50
+
+//outline around main petals
+let outlineSize = 40
+let outlineX = 50
+let outlineY = 50
+
+//outside extra petals
+let outsidePetalSize = 30
+let petalX = 32 
+let petalY = 68
+
+let complexFlowerMode = false;
+
+let deepColourMode = true;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.resolution (FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -28,53 +50,61 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
     
     strokeWeight(0.5); //line weight
     stroke (0); // colour of the line
-    
-    //added petals
-     circle (32, 29, 30)
-     circle (69, 68, 30)
-     circle (32, 69, 30)
-     circle (69, 32, 30);
 
+    if(complexFlowerMode){
+    //added petals
+     circle (petalX,petalY-39, outsidePetalSize) //top left
+     circle (petalX+ 37, petalY, outsidePetalSize) // bottom right
+     circle (petalX, petalY + 1, outsidePetalSize) // bottom left
+     circle (petalX+37, petalY-38, outsidePetalSize); //top right
+    }
 
      //petal outline
-     circle (50, 30, 40)
-     circle (30, 50, 40)
-     circle (70, 50, 40)
-     circle (50, 70, 40);
+     circle (outlineX, outlineY-20, outlineSize) //
+     circle (outlineX-20 , outlineY , outlineSize)
+     circle (outlineX+20, outlineY, outlineSize)
+     circle (outlineX, outlineY+20, outlineSize);
 
     //main petals
-      circle (50,30,35) //top petal
-      circle (30,50,35) //left petal
-      circle (70,50,35) //right petal
-      circle (50,70,35); //bottom petal
+      circle (mainpetalX, mainpetalY-20, petalSize) //top petal
+      circle (mainpetalX - 20, mainpetalY, petalSize) //left petal
+      circle (mainpetalX + 20,mainpetalY, petalSize) //right petal
+      circle (mainpetalX, mainpetalY + 20, petalSize); //bottom petal
 
+      //line details on petals
       line (13, 50, 87 ,50) //
       line (50, 87, 50, 13); //
 
 
      //centre
       fill ("#fff3d4"); //yellow colour
-      circle (50,50,30); //centre
+      circle (centreX, centreY, centreSize); //centre
     
+
+      if(deepColourMode){
       //detail in centre of flower
-      circle (51, 36, 5)
-      circle (56, 37, 5)
-      circle (60, 40, 5)
-      circle (63, 44, 5)
-      circle (64, 49, 5)
-      circle (64, 54, 5)
-      circle (62, 59, 5)
-      circle (58, 63, 5)
-      circle (53, 65, 5)
-      circle (48, 65, 5)
-      circle (43, 63, 5)
-      circle (39, 60, 5)
-      circle (36, 56, 5)
-      circle (35, 51, 5)
-      circle (36, 46, 5)
-      circle (38, 41, 5)
-      circle (42, 38, 5)
-      circle (46, 36, 5);
+      circle (detailX - 2, detailY - 4, circleSize) 
+      circle (detailX + 3, detailY - 3, circleSize)
+      circle (detailX + 7, detailY, circleSize)
+      circle (detailX + 10,detailY + 4, circleSize)
+      circle (detailX + 11, detailY + 9, circleSize)
+      circle (detailX + 11, detailY + 14, circleSize)
+      circle (detailX + 9, detailY + 19, circleSize)
+      circle (detailX + 5, detailY + 23, circleSize)
+      circle (detailX, detailY + 25, circleSize)
+      circle (detailX - 5, detailY + 25, circleSize)
+      circle (detailX - 10, detailY + 23, circleSize) 
+      circle (detailX - 14, detailY + 20, circleSize)
+      circle (detailX - 17, detailY + 16, circleSize)
+      circle (detailX - 18, detailY + 11, circleSize)
+      circle (detailX - 17, detailY + 6, circleSize)
+      circle (detailX - 15, detailY + 1, circleSize)
+      circle (detailX - 11, detailY - 2 , circleSize)
+      circle (detailX - 7 , detailY - 4, circleSize)
 
     
+
+      }
+
+
       }
