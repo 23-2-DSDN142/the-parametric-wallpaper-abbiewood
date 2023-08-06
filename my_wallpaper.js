cdd121a32a148a2725354666436014256079f0e0
abbiewood <139762394+abbiewood@users.noreply.github.com>
2023-08-06 17:38:37 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -3,15 +3,15 @@
 //centre + details
 let centreX = 50 // flower centre
 let centreY = 50 
-let centreSize = 30
+let centreSize = 50
 
 //centre circle details
 let detailX = 53
 let detailY = 40
-let circleSize = 5 //small circles around perimeter of centre
+let circleSize = 8 //small circles around perimeter of centre
 
 //main petals
-let petalSize = 35 
+let petalSize = 37
 let mainpetalX = 50
 let mainpetalY = 50
 
@@ -21,28 +21,26 @@ let outlineX = 50
 let outlineY = 50
 
 //outside extra petals
-let outsidePetalSize = 30
+let outsidePetalSize = 36
 let petalX = 32 
 let petalY = 68
 
 let complexFlowerMode = true;
 
-let deepColourMode = true;
-
-
+let softColourMode = false;
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution (NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 100;
   pWallpaper.grid_settings.cell_height = 100;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background("#1C2A4A"); //light pink colour
+  background("#CCDCD0"); //light pink colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
@@ -53,7 +51,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
     if(complexFlowerMode){
     //added petals
-    fill ("#F0A9A2")
+    fill ("#1C2A4A")
      circle (petalX,petalY-39, outsidePetalSize) //top left
      circle (petalX+ 37, petalY, outsidePetalSize) // bottom right
      circle (petalX, petalY + 1, outsidePetalSize) // bottom left
@@ -61,7 +59,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      
     }
 
-    
+  
      //petal outline
      fill (255)
      circle (outlineX, outlineY-20, outlineSize) //
@@ -70,7 +68,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      circle (outlineX, outlineY+20, outlineSize);
 
     //main petals
-      fill ("#CCDCD0")
+      fill ("#F0A9A2")
       circle (mainpetalX, mainpetalY-20, petalSize) //top petal
       circle (mainpetalX - 20, mainpetalY, petalSize) //left petal
       circle (mainpetalX + 20,mainpetalY, petalSize) //right petal
@@ -84,12 +82,12 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      //centre
       fill ("#fff3d4"); //yellow colour
       circle (centreX, centreY, centreSize); //centre
-    
 
-      if(deepColourMode){
+
+      if(softColourMode){
 
       //detail in centre of flower
-      fill ("#DB652D")
+      fill ("#DB652D") //light yellow
       circle (detailX - 2, detailY - 4, circleSize) 
       circle (detailX + 3, detailY - 3, circleSize)
       circle (detailX + 7, detailY, circleSize)
@@ -109,7 +107,54 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
       circle (detailX - 11, detailY - 2 , circleSize)
       circle (detailX - 7 , detailY - 4, circleSize)
 
-      fill ("#F0A9A2")
+      fill ("#fff3d4"); //pink colour
+    
+       //top left cluster
+      circle (6, 7, 5)
+      circle (20, 10, 7)
+      circle (10, 20, 3)
+
+       //bottom left cluster
+      circle (20, 90, 8)
+      circle (8, 86, 4)
+      circle (10, 70, 5)
+
+      //top right cluster
+      circle (93, 20, 8)
+      circle (72, 10, 5)
+      circle (90, 8, 4)
+
+      //bottom right cluster
+      circle (87, 82, 6)
+      circle (92, 92, 5)
+      circle (75, 88, 4)
+
+      }
+
+      else { 
+
+      fill ("#1C2A4A")
+      circle (detailX - 2, detailY - 4, circleSize) 
+      circle (detailX + 3, detailY - 3, circleSize)
+      circle (detailX + 7, detailY, circleSize)
+      circle (detailX + 10,detailY + 4, circleSize)
+      circle (detailX + 11, detailY + 9, circleSize)
+      circle (detailX + 11, detailY + 14, circleSize)
+      circle (detailX + 9, detailY + 19, circleSize)
+      circle (detailX + 5, detailY + 23, circleSize)
+      circle (detailX, detailY + 25, circleSize)
+      circle (detailX - 5, detailY + 25, circleSize)
+      circle (detailX - 10, detailY + 23, circleSize) 
+      circle (detailX - 14, detailY + 20, circleSize)
+      circle (detailX - 17, detailY + 16, circleSize)
+      circle (detailX - 18, detailY + 11, circleSize)
+      circle (detailX - 17, detailY + 6, circleSize)
+      circle (detailX - 15, detailY + 1, circleSize)
+      circle (detailX - 11, detailY - 2 , circleSize)
+      circle (detailX - 7 , detailY - 4, circleSize)
+
+
+      fill ("#1C2A4A"); //pink colour
     
        //top left cluster
       circle (6, 7, 4)
@@ -126,7 +171,6 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
       circle (72, 10, 2)
       circle (90, 8, 4)
 
-
       //bottom right cluster
       circle (87, 82, 4)
       circle (92, 92, 2)
@@ -135,6 +179,4 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
       }
 
-
-
-      }
+    }
